12de380eea9394152fd6b4d3434ce11ad99d1323
Gedeon Ndayiringiye <ndagedeous@gmail.com>
2024-01-29 13:12:35 +0100

    code fora  program that calculates the total amount of a meal purchased with a fixed tip.

---
+++ calculator.py
@@ -0,0 +1,15 @@
+print('input')\
+
+price = input('Charge for food:')
+
+Tip = (float(price)*18/100)
+
+Sales_Tax = (float(price)*7/100)
+
+print('output')
+
+print('Tip = :' , Tip)
+
+print('Sales Tax = :' , Sales_Tax)
+
+print('Grand Total = ', float(price) + Tip + Sales_Tax)
